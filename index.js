2ca15fcbd562293f88826bf8bce322bc4fc3470e
Rana Shravan <shravanrana228@gmail.com>
2026-06-02 15:05:44 +0530

    first commit

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ app.use(cors({
   origin: [
     "http://localhost:3000", "http://localhost:3001",
     "http://localhost:5173", "http://localhost:5174",
-    "https://venubookinguser.onrender.com",
-    "https://venuebookingadmin.onrender.com",
+    "https://venue-booking-user.onrender.com",
+    "https://venue-booking-admin.onrender.com",
       "https://your-frontend.onrender.com", // ← replace with your actual frontend URL
   ],
   credentials: true,
